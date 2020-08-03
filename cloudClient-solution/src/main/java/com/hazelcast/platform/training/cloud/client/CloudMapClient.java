01b7c3a7e03ac73c2d5bd48d0a90f27a1012d722
jsherwin <jsherwin@192.168.0.11>
2020-08-03 12:30:28 -0400

    init

---
+++ cloudClient-solution/src/main/java/com/hazelcast/platform/training/cloud/client/CloudMapClient.java
@@ -0,0 +1,45 @@
+package com.hazelcast.platform.training.cloud.client;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+/**
+ * Cloud IMDG Exercise 1 - Simply putting entries into hosted IMDG cloud service
+ *
+ */
+public class CloudMapClient {
+	private static HazelcastInstance instance;
+	
+	public CloudMapClient(){
+		CloudMapClient.init();
+	}
+	
+	private static void init() {
+		if(instance == null) {
+			instance = HazelcastClient.newHazelcastClient(Utils.getCloudClientConfig());
+		}
+	}
+	
+	public void putEntriesInToMap(){
+		try {
+			
+			IMap<String,String> map=instance.getMap("TrainingMap");
+			int i=0;
+			 while(true){
+				System.out.println("Adding Entry "+ (++i));
+				//TODO: Extra credit -- what is the difference between put and set?
+		        //String OldValue = map.put(Integer.toString(i),Double.toString(Math.random()));
+		        map.set(Integer.toString(i),Double.toString(Math.random()));
+				Thread.sleep(500);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		CloudMapClient cloudMapClient = new CloudMapClient();
+		cloudMapClient.putEntriesInToMap();
+	}
+ }
