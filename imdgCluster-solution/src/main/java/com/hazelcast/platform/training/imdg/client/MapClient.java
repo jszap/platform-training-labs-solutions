aae60d122966371221d08bfceee7195df6a3ef59
jsherwin <jsherwin@192.168.0.11>
2020-08-03 13:02:57 -0400

    init

--- imdgCluster-solution/src/main/java/com/hazelcast/platform/training/imdg/client/MapClient.java
+++ imdgCluster-solution/src/main/java/com/hazelcast/platform/training/imdg/client/MapClient.java
@@ -1,8 +1,10 @@
 package com.hazelcast.platform.training.imdg.client;
 
 import java.io.FileNotFoundException;
+import java.util.Map;
 import java.util.Random;
 
+import com.hazelcast.aggregation.Aggregators;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
@@ -18,7 +20,7 @@ public class MapClient {
 	HazelcastInstance instance;
 	
 	public MapClient() throws FileNotFoundException{
-
+		//TODO: Create a Hazelcast client instance to the the running 'PRIMARY' cluster
 		ClientConfig clientConfig = new ClientConfig();
 		clientConfig.setClusterName("PRIMARY");
 		clientConfig.getNetworkConfig().addAddress("localhost:6701");
@@ -37,7 +39,7 @@ public class MapClient {
 	private void processInput(){
 		try {
 			
-			IMap<String,String> map=instance.getMap("TrainnigMap");
+			IMap<Object, Object> map=instance.getMap("TrainnigMap");
 			Random randInt = new Random();
 			Integer.toString(randInt.nextInt(10000));
 			
@@ -46,6 +48,9 @@ public class MapClient {
 				System.out.println("Adding Entry key: "+ key);
 		        //String OldValue = map.put(key,Double.toString(Math.random()));
 		        map.set(key,Double.toString(Math.random()));
+		        
+		        getMapEntryCount(map);
+		        
 				Thread.sleep(500);
 			}
 		} catch (Exception e) {
@@ -53,6 +58,14 @@ public class MapClient {
 		}
 	}
 		
+	private static long getMapEntryCount(IMap<Object, Object> map) {
+        //Execute the aggregation compute and print the result
+		//NOTE on a distributed map, this is recommended instead of map.size()
+        long entryCount = map.aggregate(Aggregators.<Map.Entry<Object, Object>>count());
+        System.out.println("Number of entries in Map<" +map.getName()+">: " + entryCount);
+        return entryCount;
+	}
+
 	public static void main(String[] args) {
 		try {
 			MapClient mapClient = new MapClient();
